f9ef67c7a8412967942e67acfd629090eb88672f
leebar_nexfron <leebar_nexfron@ipccap.itembay.co.kr>
2018-07-30 14:48:57 +0900

    1st

---
+++ src/java_test/java_test.java
@@ -0,0 +1,12 @@
+package java_test;
+
+public class java_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!!!");
+		
+	}
+
+}
